f58076d982199caa900aaa9a906a1567cb5ca667
Arvind Singh <arvind.singh@in.fujitsu.com>
2017-03-10 17:04:14 +0530

    rectified build failure issues with scala files
    rectified build failure issues with scala files

--- src/main/scala/Serving.scala
+++ src/main/scala/Serving.scala
@@ -1,6 +1,6 @@
 package org.template.regression
 
-import io.prediction.controller.LServing
+import org.apache.predictionio.controller.LServing
 
 class Serving extends LServing[Query, PredictedResult] {
 
